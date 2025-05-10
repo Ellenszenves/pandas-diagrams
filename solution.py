108059ebffec7bbf0b2d9aa2a84963a502c37c05
Ellenszenves <88683249+Ellenszenves@users.noreply.github.com>
2025-05-10 10:40:31 +0200

    Added file checking

--- solution.py
+++ solution.py
@@ -4,6 +4,7 @@ import requests
 import csv
 import seaborn as sns
 import tkinter as tk
+import os.path
 
 def download():
     """downloading data"""
@@ -20,72 +21,93 @@ def download():
 
 def clear_data():
     """clearing data"""
-    year = []
-    total = []
-    male = []
-    woman = []
-    with open('nepesseg.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-        i = 0
-        for row in spamreader:
-            if i == 1:
-                for column in row:
-                    if column.isnumeric():
-                        year.append(column)
-            elif i == 3:
-                for column in row:
-                    if column != "férfi":
-                        male.append(column.replace(" ", ""))
-            elif i == 4:
-                for column in row:
-                    if column != "nő":
-                        woman.append(column.replace(" ", ""))
-            elif i == 5:
-                for column in row:
-                    if column != "összesen":
-                        total.append(column.replace(" ", ""))
-            i += 1
-    dict = {'year': year, 'male': male, 'woman': woman,'total': total}
-    df = pd.DataFrame(dict)
-    #print(df)
-    df.to_csv('tisztitott_nepesseg.csv', header=True, index=False)
+    if os.path.isfile("nepesseg.csv"):
+        year = []
+        total = []
+        male = []
+        woman = []
+        with open('nepesseg.csv', newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+            i = 0
+            for row in spamreader:
+                if i == 1:
+                    for column in row:
+                        if column.isnumeric():
+                            year.append(column)
+                elif i == 3:
+                    for column in row:
+                        if column != "férfi":
+                            male.append(column.replace(" ", ""))
+                elif i == 4:
+                    for column in row:
+                        if column != "nő":
+                            woman.append(column.replace(" ", ""))
+                elif i == 5:
+                    for column in row:
+                        if column != "összesen":
+                            total.append(column.replace(" ", ""))
+                i += 1
+        dict = {'year': year, 'male': male, 'woman': woman,'total': total}
+        df = pd.DataFrame(dict)
+        #print(df)
+        df.to_csv('tisztitott_nepesseg.csv', header=True, index=False)
+    else:
+        print("A nepesseg.csv file nem létezik, kérlek használd a letöltés funkciót.")
     
 def analyze():
     """statistics"""
-    df = pd.read_csv('tisztitott_nepesseg.csv')
-    print(df.head())
-    print(df.corr())
+    if os.path.isfile("tisztitott_nepesseg.csv"):
+        df = pd.read_csv('tisztitott_nepesseg.csv')
+        print(df.head())
+        print(df.corr())
+    else:
+        print("A tisztitott_nepesseg.csv file nem létezik, kérlek használd a tisztítás vagy a letöltés funkciót.")
 
 def analyze_win():
     """GUI statistics"""
-    df = pd.read_csv('tisztitott_nepesseg.csv')
-    stat_window = tk.Toplevel()
-    stat_window.title("Népesség Analizátor")
-    text_widget = tk.Text(stat_window, width=60, height=15)
-    text_widget.pack(padx=10, pady=10)
-    text_widget.insert(tk.END, df.to_string(index=False))
+    if os.path.isfile("tisztitott_nepesseg.csv"):
+        df = pd.read_csv('tisztitott_nepesseg.csv')
+        stat_window = tk.Toplevel()
+        stat_window.title("Népesség Analizátor")
+        text_widget = tk.Text(stat_window, width=60, height=15)
+        text_widget.pack(padx=10, pady=10)
+        text_widget.insert(tk.END, df.to_string(index=False))
+    else:
+        print("A tisztitott_nepesseg.csv file nem létezik, kérlek használd a tisztítás vagy a letöltés funkciót.")
 
 def line_diagram():
     """creating line-diagram"""
-    df = pd.read_csv('tisztitott_nepesseg.csv')
-    df.plot.line(x = 'year')
-    plt.title("Teljes népesség alakulása + nemek szerinti változás")
-    plt.show()
+    if os.path.isfile("tisztitott_nepesseg.csv"):
+        df = pd.read_csv('tisztitott_nepesseg.csv')
+        df.plot.line(x = 'year')
+        plt.title("Teljes népesség alakulása + nemek szerinti változás")
+        plt.show()
+    else:
+        print("A tisztitott_nepesseg.csv file nem létezik, kérlek használd a tisztítás vagy a letöltés funkciót.")
 
 def dot_diagram():
     """creating dot diagram"""
-    df = pd.read_csv('tisztitott_nepesseg.csv')
-    sns.lmplot(x='year',y='total',data=df,fit_reg=True) 
-    plt.title("Teljes népesség alakulása/lineáris regresszió")
-    plt.show()
+    if os.path.isfile("tisztitott_nepesseg.csv"):
+        df = pd.read_csv('tisztitott_nepesseg.csv')
+        sns.lmplot(x='year',y='total',data=df,fit_reg=True) 
+        plt.title("Teljes népesség alakulása/lineáris regresszió")
+        plt.show()
+    else:
+        print("A tisztitott_nepesseg.csv file nem létezik, kérlek használd a tisztítás vagy a letöltés funkciót.")
 
 def window_mode():
     """Creating GUI"""
     #Ablak
     window = tk.Tk()
     window.title("Népesség Analizátor")
-    window.geometry("250x300")
-    label = tk.Label(window, text="Nyomjon meg egy gombot!", font=("Arial", 14))
+    window.geometry("750x300")
+    if os.path.isfile("nepesseg.csv"):
+        if os.path.isfile("tisztitott_nepesseg.csv"):
+            label = tk.Label(window, text="Nyomjon meg egy gombot!", font=("Arial", 14))
+        else:
+            label = tk.Label(window, text="A tisztitott_nepesseg.csv file nem létezik, kérlek használd a tisztítás vagy a letöltés funkciót.", font=("Arial", 14))
+    else:
+        label = tk.Label(window, text="A nepesseg.csv file nem létezik, kérlek használd a letöltés funkciót.", font=("Arial", 14))
     label.pack(pady=10)
     #Gombok
     download_button = tk.Button(window, text="Letöltés", command=download)
@@ -100,8 +122,9 @@ def window_mode():
     line_button.pack(pady=5)
     exit_button = tk.Button(window, text="Kilépés", command=exit)
     exit_button.pack(pady=5)
-    window.mainloop()
 
+    window.mainloop()
+    
 def main_menu():
     """interactive menu"""
     print("Hello! Funkciók:\n 1. Letöltés\n 2. Tisztítás\n 3. Statisztika\n 4. Pont-diagram\n 5. Vonal-diagram\n 6. Grafikus felület\n 7. Kilépés")
